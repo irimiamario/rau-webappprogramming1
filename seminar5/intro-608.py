7de4c813aa9520d2f3fca59ec0c4d11fc0fbfc6e
Andrei Luchici <andrei.luchici@daciandata.tech>
2021-11-03 12:06:21 +0200

    update folder structure + added seminar 5 G608

---
+++ seminar5/intro-608.py
@@ -0,0 +1,8 @@
+value = input("Please enter a value: ")
+print(type(value), value)
+
+value = int(value)  # numar intreg
+print(type(value), value)
+
+value = float(value)  # numar zecimal
+print(type(value), value)

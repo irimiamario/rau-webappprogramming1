0375a3c9b20fc650ee85565b8afb14fc6715441a
Andrei Luchici <andrei.luchici@daciandata.tech>
2021-11-03 15:21:56 +0200

    added seminar 5 G612

---
+++ seminar5/intro_612.py
@@ -0,0 +1,18 @@
+n = input("Please enter a number: ")
+print(type(n), n)
+
+n = int(n)
+print(type(n), n)
+
+n = float(n)
+print(type(n), n)
+
+s = "This is a string"
+parts = s.split(" ")
+print(parts)
+
+parts = s.split("is")
+print(parts)
+
+a = 13 % 2
+print(a)

9bbddf533cc06dc77bea1b3b4996a41748378b4b
Andrei Luchici <andrei.luchici@daciandata.tech>
2021-12-02 11:17:49 +0200

    seminar 9 wip

--- user_management_api/repository.py
+++ user_management_api/repository.py
@@ -38,3 +38,40 @@ def get_user_email_and_pass(conn, username):
     cursor = conn.cursor()
     results = list(cursor.execute(query))
     return results[0]
+
+
+# delete
+def delete_user(conn, user_id=None, username=None):
+    query = ""
+    if user_id is not None:
+        query = f"delete from users where id = {user_id}"
+    if username is not None:
+        query = f"delete from users where username = '{username}'"
+    cursor = conn.cursor()
+    cursor.execute(query)
+    conn.commit()
+
+
+# update
+def update_user(conn, user_id=None, username=None, details=None):
+    """
+    UPDATE table_name
+    SET column1 = value1, column2 = value2...., columnN = valueN
+    WHERE [condition];
+
+    details = {
+    "email": "a@c.com"
+    }
+    """
+    set_statement = ""
+    for key, value in details.items():
+        set_statement = set_statement + f"{key}={value},"
+    set_statement = set_statement[:-1]
+    query = ''
+    if user_id is not None:
+        query = f"update users set {set_statement} where id = {user_id}"
+    if username is not None:
+        query = f"update users set {set_statement} where username = '{username}'"
+    cursor = conn.cursor()
+    cursor.execute(query)
+    conn.commit()
